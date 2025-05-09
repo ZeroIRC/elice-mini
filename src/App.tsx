80c184f9a85e54e12e3e110a0719b1976b713ae6
ZeroIRC <save3172@gmail.com>
2025-05-09 14:45:25 +0900

    fix: 기본레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './index.css'
-
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">Hello World</h1>
+    <div className="h-screen flex flex-col">
+      {/* 헤더 - Zip File Editor */}
+      <div className="flex-none bg-gray-800 text-white p-4 border-b border-gray-600">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-bold">ZIP File Editor</h1>
+        </div>
+      </div>
+
+      {/* 메인 콘텐츠 영역 */}
+      <div className="flex-1 flex">
+        {/* 왼쪽 - 파일 트리 */}
+        <div className="w-64 border-r border-gray-600 bg-gray-800"></div>
+
+        {/* 오른쪽 영역 */}
+        <div className="flex-1 flex flex-col">
+          {/* 탭 바 */}
+
+          {/* Monaco 에디터 영역 */}
+          <div className="flex-1 bg-[#1e1e1e]"></div>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default App
