a585cb1a6a976094a202ef1d1520f3cf5018cbeb
ZeroIRC <save3172@gmail.com>
2025-05-09 15:20:47 +0900

    feat: App 파일트리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
+import { FileTree } from './components/FileTree'
+import { useFileStore } from './store/useFileStore'
+import { useFileActions } from './hooks/useFileActions'
+import { useTreeActions } from './hooks/useTreeActions'
+
 export default function App() {
+  const { root, selectedNode, expandedPaths } = useFileStore()
+  const { handleUpload, handleDownload } = useFileActions()
+  const { toggleExpand, selectFile } = useTreeActions()
+
   return (
     <div className="h-screen flex flex-col">
       {/* 헤더 - Zip File Editor */}
       <div className="flex-none bg-gray-800 text-white p-4 border-b border-gray-600">
         <div className="flex items-center justify-between">
           <h1 className="text-xl font-bold">ZIP File Editor</h1>
+          <div className="flex gap-4">
+            <input
+              type="file"
+              accept=".zip"
+              onChange={handleUpload}
+              className="text-sm text-gray-300
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-md file:border-0
+                file:text-sm file:font-semibold
+                file:bg-gray-700 file:text-white
+                hover:file:bg-gray-600"
+            />
+            <button
+              onClick={handleDownload}
+              disabled={!root}
+              className="py-2 px-4 rounded-md text-sm font-semibold
+                bg-blue-600 text-white hover:bg-blue-700
+                disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Download ZIP
+            </button>
+          </div>
         </div>
       </div>
 
       {/* 메인 콘텐츠 영역 */}
       <div className="flex-1 flex">
         {/* 왼쪽 - 파일 트리 */}
-        <div className="w-64 border-r border-gray-600 bg-gray-800"></div>
+        <div className="w-64 border-r border-gray-600 bg-gray-800">
+          <FileTree
+            root={root}
+            selectedNode={selectedNode}
+            expandedPaths={expandedPaths}
+            onSelect={selectFile}
+            onToggleExpand={toggleExpand}
+          />
+        </div>
 
         {/* 오른쪽 영역 */}
         <div className="flex-1 flex flex-col">
