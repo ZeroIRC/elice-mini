fe7bd80de4aa9e18c01d41caf511eb051fa37733
ZeroIRC <save3172@gmail.com>
2025-05-09 15:16:44 +0900

    feat: useFileStore 전역스토어 추가

---
+++ src/store/useFileStore.ts
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+import { FileStore } from '../types/FileStore'
+
+export const useFileStore = create<FileStore>(() => ({
+  root: null,
+  selectedNode: null,
+  expandedPaths: new Set(['/']) as Set<string>,
+  originalFileName: '',
+  selectedFile: null,
+  openFiles: new Map(),
+  isDirty: new Map(),
+}))
